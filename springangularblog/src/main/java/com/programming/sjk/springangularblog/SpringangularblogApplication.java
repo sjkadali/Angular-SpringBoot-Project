b433417a15115d9e4183b368f67ef084448d8ada
sjkadali <skadalikat@gmail.com>
2020-11-11 16:10:08 -0500

    Setup Updates

---
+++ springangularblog/src/main/java/com/programming/sjk/springangularblog/SpringangularblogApplication.java
@@ -0,0 +1,13 @@
+package com.programming.sjk.springangularblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringangularblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringangularblogApplication.class, args);
+	}
+
+}
---
+++ springangularblog/src/test/java/com/programming/sjk/springangularblog/SpringangularblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.sjk.springangularblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringangularblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
